2059697a9e33c6fb344856d6fa28eab2f05aecee
Yoan Le Clanche <yoanl@pilotsystems.net>
2022-10-11 16:28:21 +0200

    Fix typo

--- quiz/forms.py
+++ quiz/forms.py
@@ -5,7 +5,7 @@ from django.forms.widgets import RadioSelect, Textarea
 class QuestionForm(forms.Form):
     def __init__(self, question, *args, **kwargs):
         super(QuestionForm, self).__init__(*args, **kwargs)
-        if not self.question:
+        if not question:
             return
         choice_list = [x for x in question.get_answers_list()]
         self.fields["answers"] = forms.ChoiceField(choices=choice_list,
